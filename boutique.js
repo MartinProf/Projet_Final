c70592859fe44d7e5e52fdb1146f35ba49668f63
Martin Forget <mforget2011@hotmail.com>
2022-09-11 11:31:24 -0400

    js boutique, part de là.. je trouve pas.

--- boutique.js
+++ boutique.js
@@ -1,40 +1,32 @@
 
-
 let total;
 
 
 // Increasing Function
-function increaseItem(param, itemPrice) {
+function increaseItem(param , itemPrice){
 
     let noOfItem = document.getElementById(param);
-    let itemsPrice = document.getElementById(itemPrice).innerHTML.replace('$', '');
+    let itemsPrice = document.getElementById(itemPrice).innerHTML.replace('$' , '');
 
-
-    noOfItem.value = parseInt(noOfItem.value) + 1;
-    total = (noOfItem.value) * parseInt(itemsPrice);
-    console.log(total);
-    document.getElementById('total').innerHTML = total;
-    document.getElementById('numOfItem').innerHTML = noOfItem.value;
-    
-
+        noOfItem.value = parseInt(noOfItem.value) + 1;
+        total = document.getElementById('userTotalAmount').innerHTML = (noOfItem.value) * parseInt(itemsPrice);
+        console.log(total);
+        document.getElementById('numOfItem').innerHTML = noOfItem.value;
 }
 
 // Decreasing Function
-function decreaseItem(param, itemPrice) {
+function decreaseItem(param , itemPrice){
 
     let noOfItem = document.getElementById(param);
-    let itemsPrice = document.getElementById(itemPrice).innerHTML.replace('$', '');
+    let itemsPrice = document.getElementById(itemPrice).innerHTML.replace('$' , '');
 
-
-
-    if (noOfItem.value <= 0) {
-        noOfItem.value = 0;
-        alert('Vous devez sélectionner au moins 1 item');
+        if (noOfItem.value <= 0) {
+            noOfItem.value = 0;
+            alert('Oder Can"t Be Negative');
+        }
+        else{
+            noOfItem.value = parseInt(noOfItem.value) - 1;
+            total = document.getElementById('userTotalAmount').innerHTML = (noOfItem.value) * itemsPrice;
+            document.getElementById('numOfItem').innerHTML = noOfItem.value;
+        }
     }
-    else {
-        noOfItem.value = parseInt(noOfItem.value) - 1;
-        total = (noOfItem.value) * itemsPrice;
-        document.getElementById('total').innerHTML = total;
-        document.getElementById('numOfItem').innerHTML = noOfItem.value;
-    }
-}
