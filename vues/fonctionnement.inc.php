a441d1b697cb073368293a6d4bce1be1e84112ec
Martin Forget <mforget2011@hotmail.com>
2022-09-14 20:18:40 -0400

    Squashed commit of the following:
    commit f5ddb902e431a2c551f60597a500ecd76ee0cef6
    Author: MartinProf <crosemont.martin@gmail.com>
    Date:   Wed Sep 14 19:55:26 2022 -0400
    
        Ajout du PHP

---
+++ vues/fonctionnement.inc.php
@@ -0,0 +1,51 @@
+<?php
+echo '
+<section class="banner pt-5" id="couleurDropDown">
+<div class="container pt-5">
+    <div class="row pt-5 d-flex flex-row wrapper">
+        <div class="col-6 d-flex justify-content-center align-items-center">
+            <img src="/images/Agriculture_biologique500x500.jpeg" alt="" style="height: 300px;" class="img-responsive imageRonde bg-info">
+        </div>
+        <div class="col-6 d-flex justify-content-center align-items-center p-5">
+            <div class="card text-bg-info mb-3" >
+                <div class="card-header">Boutique</div>
+                <div class="card-body">
+                  <h5 class="card-title">Information sur la boutique</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quis eius cupiditate quisquam impedit consequatur nihil velit eum illum soluta, corrupti facilis numquam nisi deleniti rem dolorum saepe neque non quas quos! Aut incidunt nesciunt ad, quos delectus ipsam dolores labore doloribus, doloremque soluta laudantium aliquam magni ut veritatis ipsa.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row pt-5 d-flex flex-row wrapper">
+        <div class="col-6 d-flex justify-content-center align-items-center">
+            <img src="/images/Agriculture_biologique500x500.jpeg" alt="" style="height: 300px;" class="img-responsive imageRonde bg-info">
+        </div>
+        <div class="col-6 d-flex justify-content-center align-items-center p-5">
+            <div class="card text-bg-info mb-3" >
+                <div class="card-header">Boutique</div>
+                <div class="card-body">
+                  <h5 class="card-title">Information sur la boutique</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quis eius cupiditate quisquam impedit consequatur nihil velit eum illum soluta, corrupti facilis numquam nisi deleniti rem dolorum saepe neque non quas quos! Aut incidunt nesciunt ad, quos delectus ipsam dolores labore doloribus, doloremque soluta laudantium aliquam magni ut veritatis ipsa.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row pt-5 d-flex flex-row wrapper" style="height: 2000;">
+        <div class="col-6 d-flex justify-content-center align-items-center">
+            <img src="/images/Agriculture_biologique500x500.jpeg" alt="" style="height: 300px;" class="img-responsive imageRonde bg-info">
+        </div>
+        <div class="col-6 d-flex justify-content-center align-items-center p-5">
+            <div class="card text-bg-info mb-3" >
+                <div class="card-header">Boutique</div>
+                <div class="card-body">
+                  <h5 class="card-title">Information sur la boutique</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quis eius cupiditate quisquam impedit consequatur nihil velit eum illum soluta, corrupti facilis numquam nisi deleniti rem dolorum saepe neque non quas quos! Aut incidunt nesciunt ad, quos delectus ipsam dolores labore doloribus, doloremque soluta laudantium aliquam magni ut veritatis ipsa.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+';
+
+?>
