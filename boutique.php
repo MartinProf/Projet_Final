7de96d8b5d8cc0e1b0f28d72cea2786e5c1ab324
MartinProf <crosemont.martin@gmail.com>
2022-09-22 18:43:01 -0400

    merge de la boutique pour avoir dequoi de fonctionel

--- boutique.php
+++ boutique.php
@@ -50,85 +50,95 @@ include_once "vues/inc/navbar.inc.php";
     }
   }
 ?>
+</div>
+
 <!---Légumes-->
 <div class="container-fluid foodwrap"></div>
 <div class="container-fluid w-100" id="legumes"></div>
 <div class="container-fluid foodwrap"></div>
-<div class="container-fluid d-inline-flex justify-content-center products">
-<?php
 
-   for ($i=0; $i < count($tableauArticles); $i++) { 
-    if ($tableauArticles[$i]->getIdArticle() == 2) {
-      echo '
-      <div class="product-item">
-        <div class="product-img">
-          <img src="'.$tableauArticles[$i]->getImage_location().'" alt="'.$tableauArticles[$i]->getArticle().'" />
-        </div>
-        <div class="product-description">
-          <h4 class="product-name"><a href="#">'.$tableauArticles[$i]->getArticle().'</a></h4>
-          <p id="prix'.$tableauArticles[$i]->getArticle().'">' . $tableauArticles[$i]->getPrix() . '</p>
-          <div class="buttons">
-            <button class="btn-minus" onclick="decreaseItem(\''.$tableauArticles[$i]->getArticle().'\' , \'prix'.$tableauArticles[$i]->getArticle().'\')">-</button>
-            <input type="text" id="'.$tableauArticles[$i]->getArticle().'" value="0" />
-            <button class="btn-plus" onclick="increaseItem(\''.$tableauArticles[$i]->getArticle().'\' , \'prix'.$tableauArticles[$i]->getArticle().'\')">+</button>
+<div class="container-fluid d-inline-flex justify-content-center products">
+  <?php
+
+    for ($i=0; $i < count($tableauArticles); $i++) { 
+      if ($tableauArticles[$i]->getIdArticle() == 2) {
+        echo '
+        <div class="product-item">
+          <div class="product-img">
+            <img src="'.$tableauArticles[$i]->getImage_location().'" alt="'.$tableauArticles[$i]->getArticle().'" />
           </div>
-        </div>
-      </div>';
+          <div class="product-description">
+            <h4 class="product-name"><a href="#">'.$tableauArticles[$i]->getArticle().'</a></h4>
+            <p id="prix'.$tableauArticles[$i]->getArticle().'">' . $tableauArticles[$i]->getPrix() . '</p>
+            <div class="buttons">
+              <button class="btn-minus" onclick="decreaseItem(\''.$tableauArticles[$i]->getArticle().'\' , \'prix'.$tableauArticles[$i]->getArticle().'\')">-</button>
+              <input type="text" id="'.$tableauArticles[$i]->getArticle().'" value="0" />
+              <button class="btn-plus" onclick="increaseItem(\''.$tableauArticles[$i]->getArticle().'\' , \'prix'.$tableauArticles[$i]->getArticle().'\')">+</button>
+            </div>
+          </div>
+        </div>';
+      }
     }
-  }
-?>
+  ?>
+</div>
 <!---Viandes-->
 <div class="container-fluid foodwrap"></div>
 <div class="container-fluid w-100" id="viandes"></div>
 <div class="container-fluid foodwrap"></div>
+
 <div class="container-fluid d-inline-flex justify-content-center products">
-<?php
-   for ($i=0; $i < count($tableauArticles); $i++) { 
-    if ($tableauArticles[$i]->getIdArticle() == 3) {
-      echo '
-      <div class="product-item">
-        <div class="product-img">
-          <img src="'.$tableauArticles[$i]->getImage_location().'" alt="'.$tableauArticles[$i]->getArticle().'" />
-        </div>
-        <div class="product-description">
-          <h4 class="product-name"><a href="#">'.$tableauArticles[$i]->getArticle().'</a></h4>
-          <p id="prix'.$tableauArticles[$i]->getArticle().'">' . $tableauArticles[$i]->getPrix() . '</p>
-          <div class="buttons">
-            <button class="btn-minus" onclick="decreaseItem(\''.$tableauArticles[$i]->getArticle().'\' , \'prix'.$tableauArticles[$i]->getArticle().'\')">-</button>
-            <input type="text" id="'.$tableauArticles[$i]->getArticle().'" value="0" />
-            <button class="btn-plus" onclick="increaseItem(\''.$tableauArticles[$i]->getArticle().'\' , \'prix'.$tableauArticles[$i]->getArticle().'\')">+</button>
+  <?php
+    for ($i=0; $i < count($tableauArticles); $i++) { 
+      if ($tableauArticles[$i]->getIdArticle() == 3) {
+        echo '
+        <div class="product-item">
+          <div class="product-img">
+            <img src="'.$tableauArticles[$i]->getImage_location().'" alt="'.$tableauArticles[$i]->getArticle().'" />
           </div>
-        </div>
-      </div>';
+          <div class="product-description">
+            <h4 class="product-name"><a href="#">'.$tableauArticles[$i]->getArticle().'</a></h4>
+            <p id="prix'.$tableauArticles[$i]->getArticle().'">' . $tableauArticles[$i]->getPrix() . '</p>
+            <div class="buttons">
+              <button class="btn-minus" onclick="decreaseItem(\''.$tableauArticles[$i]->getArticle().'\' , \'prix'.$tableauArticles[$i]->getArticle().'\')">-</button>
+              <input type="text" id="'.$tableauArticles[$i]->getArticle().'" value="0" />
+              <button class="btn-plus" onclick="increaseItem(\''.$tableauArticles[$i]->getArticle().'\' , \'prix'.$tableauArticles[$i]->getArticle().'\')">+</button>
+            </div>
+          </div>
+        </div>';
+      }
     }
-  }
-?>
+  ?>
+</div>
 <!---Autres-->
 <div class="container-fluid foodwrap"></div>
 <div class="container-fluid w-100" id="diversProd"></div>
 <div class="container-fluid foodwrap"></div>
-<div class="container-fluid d-inline-flex justify-content-center products" style="margin-bottom: 206px;">
+
+<div class="container-fluid d-inline-flex justify-content-center products">
   <?php
-   for ($i=0; $i < count($tableauArticles); $i++) { 
-    if ($tableauArticles[$i]->getIdArticle() == 4) {
-      echo '
-      <div class="product-item">
-        <div class="product-img">
-          <img src="'.$tableauArticles[$i]->getImage_location().'" alt="'.$tableauArticles[$i]->getArticle().'" />
-        </div>
-        <div class="product-description">
-          <h4 class="product-name"><a href="#">'.$tableauArticles[$i]->getArticle().'</a></h4>
-          <p id="prix'.$tableauArticles[$i]->getArticle().'">' . $tableauArticles[$i]->getPrix() . '</p>
-          <div class="buttons">
-            <button class="btn-minus" onclick="decreaseItem(\''.$tableauArticles[$i]->getArticle().'\' , \'prix'.$tableauArticles[$i]->getArticle().'\')">-</button>
-            <input type="text" id="'.$tableauArticles[$i]->getArticle().'" value="0" />
-            <button class="btn-plus" onclick="increaseItem(\''.$tableauArticles[$i]->getArticle().'\' , \'prix'.$tableauArticles[$i]->getArticle().'\')">+</button>
-          </div>
-        </div>
-      </div>';
+    for ($i=0; $i < count($tableauArticles); $i++) { 
+      if ($tableauArticles[$i]->getIdArticle() == 4) {
+        echo '
+          <div class="product-item">
+            <div class="product-img">
+              <img src="'.$tableauArticles[$i]->getImage_location().'" alt="'.$tableauArticles[$i]->getArticle().'" />
+            </div>
+            <div class="product-description">
+              <h4 class="product-name"><a href="#">'.$tableauArticles[$i]->getArticle().'</a></h4>
+              <p id="prix'.$tableauArticles[$i]->getArticle().'">' . $tableauArticles[$i]->getPrix() . '</p>
+              <div class="buttons">
+                <button class="btn-minus" onclick="decreaseItem(\''.$tableauArticles[$i]->getArticle().'\' , \'prix'.$tableauArticles[$i]->getArticle().'\')">-</button>
+                <input type="text" id="'.$tableauArticles[$i]->getArticle().'" value="0" />
+                <button class="btn-plus" onclick="increaseItem(\''.$tableauArticles[$i]->getArticle().'\' , \'prix'.$tableauArticles[$i]->getArticle().'\')">+</button>
+              </div>
+            </div>
+          </div>';
+      }
     }
-  }
-?>
+  ?>
+</div>
+
+
 
 <?php
 include_once "vues/inc/footer.inc.php";
