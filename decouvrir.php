67eb0c67b2db94580d11d7f54db40b3c98ce4a42
Martin Forget <mforget2011@hotmail.com>
2022-09-22 00:07:15 -0400

    ben des affaires!

--- decouvrir.php
+++ decouvrir.php
@@ -1,8 +1,8 @@
-<?php 
-    $titre = "Épicerie biologique";
-    $nav = "decouvrir";
-    include_once "vues/inc/entete.inc.php";
-    include_once "vues/inc/navbar.inc.php";
+<?php
+$titre = "Épicerie biologique";
+$nav = "decouvrir";
+include_once "vues/inc/entete.inc.php";
+include_once "vues/inc/navbar.inc.php";
 ?>
 
 <section class="banner d-flex justify-content-center align-items-center pt-5">
@@ -10,8 +10,8 @@
         <div class="row">
             <div class="col-2"></div>
             <div class="col-8 text-center banner-desc">
-                <h1>Bienvenue chez vous!</h1>
-                <h1>La seul épicerie biologique en ligne sans but lucratif</h1>
+                <h1 class="display-2">Bienvenue chez vous!</h1>
+                <h1 class="display-5">La seul épicerie biologique en ligne sans but lucratif</h1>
             </div>
             <div class="col-2"></div>
         </div>
@@ -21,12 +21,13 @@
 <div class="container-fluid foodwrap"></div>
 
 <section class="py-5" id="couleurDropDown">
-    <div class="container" >
+    <div class="container">
         <div class="row">
             <div class="col-lg-6">
                 <img src="images/Champs_bio.jpeg" class="img-fluid rounded-3" alt="">
             </div>
-            <div class="col-lg-6">
+            <div class="col-lg-1"></div>
+            <div class="col-lg-5">
                 <h1>Pourquoi nous existons?</h1>
                 <p>Nous sommes guidé par notre raison d’être, qui est de favoriser la transition de notre société vers des écosystèmes en santé et durables à travers l’alimentation biologique et éthique. Nous nous y prenons en rendant accessibles des produits biologiques non périssables d’excellente qualité tout en créant un impact positif sur la Terre et sur ses habitants!</p>
             </div>
@@ -36,74 +37,73 @@
 
 
 <section class="py-5" id="couleurDropDown">
-<div class="container" >
-    <div class="row">
-        <div class="col-lg-6">
-            <h1>Comment sommes nous né?</h1>
-            <p>Adam, Valérie et Mylène, trois amis et colocataires, trouvaient que leur épicerie biologique leur coûtait trop cher, en plus de générer beaucoup d’emballages. Ils ont donc eu l’idée géniale de s’approvisionner directement chez les producteurs et fournisseurs de leurs produits favoris.
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5">
+                <h1>Comment sommes nous né?</h1>
+                <p>Adam, Valérie et Mylène, trois amis et colocataires, trouvaient que leur épicerie biologique leur coûtait trop cher, en plus de générer beaucoup d’emballages. Ils ont donc eu l’idée géniale de s’approvisionner directement chez les producteurs et fournisseurs de leurs produits favoris.
 
-                « Mais que faire avec toute cette chaudière de beurre de peanuts? Et ces poches pleines de riz, de spaghettis et de haricots? Et cette caisse de chocolat? » se demanda Adam lorsque sa première commande a été livrée. « On partage avec notre communauté! » lui répondirent Valérie et Mylène! Voilà comment les trois amis ont cofondé le groupe d’achat NousRire.
-                
-                En mai 2015, cette idée fut semée dans un Québec fertile ayant soif d’alternatives saines, justes et écoresponsables.
-                
-                Infinis mercis et gratitude à tous nos participants·e·s et bénévoles!
+                    « Mais que faire avec toute cette chaudière de beurre de peanuts? Et ces poches pleines de riz, de spaghettis et de haricots? Et cette caisse de chocolat? » se demanda Adam lorsque sa première commande a été livrée. « On partage avec notre communauté! » lui répondirent Valérie et Mylène! Voilà comment les trois amis ont cofondé le groupe d’achat NousRire.
+
+                    En mai 2015, cette idée fut semée dans un Québec fertile ayant soif d’alternatives saines, justes et écoresponsables.
+
+                    Infinis mercis et gratitude à tous nos participants·e·s et bénévoles!
                 </p>
+            </div>
+            <div class="col-lg-2"></div>
+            <div class="col-lg-5">
+                <img src="images/plantePousse.gif" class="img-fluid rounded-3 w-100" alt="pousse">
+            </div>
         </div>
-        <div class="col-lg-6">
-            <img src="images/Agriculture_biologique.jpeg" class="img-fluid rounded-3" alt="">
-        </div>
-
     </div>
-</div>
 </section>
 
 <div class="container-fluid foodwrap"></div>
 
 <section class="banner2 d-flex justify-content-center align-items-center pt-5">
-<div class="container">
-    <div class="row">
-        <div class="col-2"></div>
-        <div class="col-8 text-center banner2-desc">
-            <h1>Bienvenue chez vous!</h1>
-            <h1>La seul épicerie en ligne sans but lucratif</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-2"></div>
+            <div class="col-8 text-center banner2-desc">
+                <h1 class="display-1">Chez vous, c'est...</h1>
+            </div>
+            <div class="col-2"></div>
         </div>
-        <div class="col-2"></div>
     </div>
-</div>
 </section>
 
 <div class="container-fluid foodwrap"></div>
 
 <section class="py-5" id="couleurDropDown">
-<div class="container">
-    <div class="d-flex flex-wrap justify-content-center gap-5">
-        <div class="imageFond m-3 bg-body rounded-3" style="width: 250px; height: 350px;">
-            <div class="d-flex flex-column h-100 justify-content-center align-content-center">
-                <div class="text-center">Des produits d'une fraîcheur exceptionnelle</div>
+    <div class="container">
+        <div class="d-flex flex-wrap justify-content-center gap-5">
+            <div class="imageFond m-3 bg-body rounded-3" style="width: 250px; height: 350px;">
+                <div class="d-flex flex-column h-100 justify-content-center align-content-center">
+                    <div class="text-center">Des produits d'une fraîcheur exceptionnelle</div>
+                </div>
             </div>
-        </div>
-        <div class="imageFond m-3 bg-body rounded-3" style="width: 250px; height: 350px;">
-            <div class="d-flex flex-column h-100 justify-content-center align-content-center">
-                <div class="texteImage text-center">Des produits d'une fraîcheur exceptionnelle</div>
+            <div class="imageFond m-3 bg-body rounded-3" style="width: 250px; height: 350px;">
+                <div class="d-flex flex-column h-100 justify-content-center align-content-center">
+                    <div class="texteImage text-center">Des produits d'une fraîcheur exceptionnelle</div>
+                </div>
             </div>
-        </div>
-        <div class="imageFond m-3 bg-body rounded-3" style="width: 250px; height: 350px;">
-            <div class="d-flex flex-column h-100 justify-content-center align-content-center">
-                <div class="text-center">Des produits d'une fraîcheur exceptionnelle</div>
+            <div class="imageFond m-3 bg-body rounded-3" style="width: 250px; height: 350px;">
+                <div class="d-flex flex-column h-100 justify-content-center align-content-center">
+                    <div class="text-center">Des produits d'une fraîcheur exceptionnelle</div>
+                </div>
             </div>
-        </div>
-        <div class="imageFond m-3 bg-body rounded-3" style="width: 250px; height: 350px;">
-            <div class="d-flex flex-column h-100 justify-content-center align-content-center">
-                <div class="text-center">Des produits d'une fraîcheur exceptionnelle</div>
+            <div class="imageFond m-3 bg-body rounded-3" style="width: 250px; height: 350px;">
+                <div class="d-flex flex-column h-100 justify-content-center align-content-center">
+                    <div class="text-center">Des produits d'une fraîcheur exceptionnelle</div>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </section>
 
 <div class="container-fluid foodwrap"></div>
 
-<?php 
-    include_once "vues/inc/footer.inc.php";
-    include_once "vues/inc/piedPage.inc.php";
+<?php
+include_once "vues/inc/footer.inc.php";
+include_once "vues/inc/piedPage.inc.php";
 ?>
--- fonctionnement.php
+++ fonctionnement.php
@@ -1,80 +1,106 @@
-<?php 
-    $titre = "Notre fonctionnement";
-    $nav = "fonctionnement";
-    include_once "vues/inc/entete.inc.php";
-    include_once "vues/inc/navbar.inc.php";
+<?php
+$titre = "Notre fonctionnement";
+$nav = "fonctionnement";
+include_once "vues/inc/entete.inc.php";
+include_once "vues/inc/navbar.inc.php";
 ?>
 
 <section class="banner d-flex justify-content-center align-items-center pt-5">
     <div class="container">
         <div class="row">
-            <div class="col-2"></div>
-            <div class="col-8 text-center banner-desc">
-                <h1>Bienvenue chez vous!</h1>
-                <h1>La seul épicerie biologique en ligne sans but lucratif</h1>
+            <div class="col-1"></div>
+            <div class="col-10 text-center banner-desc">
+                <h1 class="display-1">Bienvenue chez vous!</h1>
+                <h1 class="display-5">La seul épicerie biologique <br>en ligne<br> sans but lucratif</h1>
             </div>
-            <div class="col-2"></div>
+            <div class="col-1"></div>
         </div>
     </div>
 </section>
-
 <div class="container-fluid foodwrap"></div>
+
+<div class="wrapper">
+      <div class="carousel">
+        <input type="checkbox" name="" id="dot">
+        <label for="dot" class="dot1"></label>
+        <label for="dot" class="dot2"></label>
+        <div class="cardd cards-1"><img src="images/bacon.png" alt="" class="carddImg"></div>
+        <div class="cardd cards-2"><img src="images/bacon.png" alt="" class="carddImg"></div>
+        <div class="cardd cards-3"><img src="images/bacon.png" alt="" class="carddImg"></div>
+        <div class="cardd cards-4"><img src="images/bacon.png" alt="" class="carddImg"></div>
+        <div class="cardd cards-5"><img src="images/bacon.png" alt="" class="carddImg"></div>
+        <div class="cardd cards-6"><img src="images/bacon.png" alt="" class="carddImg"></div>
+        <div class="cardd cards-7"><img src="images/bacon.png" alt="" class="carddImg"></div>
+        <div class="cardd cards-8"><img src="images/bacon.png" alt="" class="carddImg"></div>
+        <div class="cardd cards-9"><img src="images/bacon.png" alt="" class="carddImg"></div>
+        <div class="cardd cards-10"><img src="images/bacon.png" alt="" class="carddImg"></div>
+      </div>
+    </div>
+
+
+
+
 
 <section class="py-5" id="couleurDropDown">
     <div class="container">
-        <div class="row pt-5 d-flex flex-row wrapper">
-            <div class="col-6 d-flex justify-content-center align-items-center">
-                <img src="images/Agriculture_biologique500x500.jpeg" alt="" style="height: 300px;" class="img-responsive imageRonde bg-info">
+        <div class="row pt-5 d-flex flex-row desc">
+            <div class="col-4 d-flex justify-content-center align-items-center imageConteneur">
+                <img src="images/champs.gif" alt="" style="height: 285px;" class="img-responsive imageRonde">
             </div>
             <div class="col-6 d-flex justify-content-center align-items-center p-5">
-                <div class="card text-bg-info mb-3" >
-                    <div class="card-header">Boutique</div>
+                <div class="card mb-3 textAnim">
+                    <div class="card-header">Des employés et bénévoles fiers et investis</div>
                     <div class="card-body">
-                      <h5 class="card-title">Information sur la boutique</h5>
-                      <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quis eius cupiditate quisquam impedit consequatur nihil velit eum illum soluta, corrupti facilis numquam nisi deleniti rem dolorum saepe neque non quas quos! Aut incidunt nesciunt ad, quos delectus ipsam dolores labore doloribus, doloremque soluta laudantium aliquam magni ut veritatis ipsa.</p>
+                        <h5 class="card-title">Information sur la boutique</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quis eius cupiditate quisquam impedit consequatur nihil velit eum illum soluta, corrupti facilis numquam nisi deleniti rem dolorum saepe neque non quas quos! Aut incidunt nesciunt ad, quos delectus ipsam dolores labore doloribus, doloremque soluta laudantium aliquam magni ut veritatis ipsa.</p>
                     </div>
                 </div>
+            </div>
+            <div class="col-2"></div>
+        </div>
+
+        <div class="row pt-5 d-flex flex-row desc">
+            <div class="col-2"></div>
+            <div class="col-6 d-flex justify-content-center align-items-center p-5">
+                <div class="card mb-3 textAnim">
+                    <div class="card-header">Boutique</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Information sur la boutique</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quis eius cupiditate quisquam impedit consequatur nihil velit eum illum soluta, corrupti facilis numquam nisi deleniti rem dolorum saepe neque non quas quos! Aut incidunt nesciunt ad, quos delectus ipsam dolores labore doloribus, doloremque soluta laudantium aliquam magni ut veritatis ipsa.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4 d-flex justify-content-center align-items-center imageConteneur">
+                <img src="images/tomateCueillie.gif" alt="" style="height: 285px;" class="img-responsive imageRonde ">
             </div>
         </div>
 
-        <div class="row pt-5 d-flex flex-row wrapper" style="height: 2000;">
-            <div class="col-6 d-flex justify-content-center align-items-center">
-                <img src="images/Agriculture_biologique500x500.jpeg" alt="" style="height: 300px;" class="img-responsive imageRonde bg-info">
-            </div>
-            <div class="col-6 d-flex justify-content-center align-items-center p-5">
-                <div class="card text-bg-info mb-3" >
-                    <div class="card-header">Boutique</div>
-                    <div class="card-body">
-                      <h5 class="card-title">Information sur la boutique</h5>
-                      <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quis eius cupiditate quisquam impedit consequatur nihil velit eum illum soluta, corrupti facilis numquam nisi deleniti rem dolorum saepe neque non quas quos! Aut incidunt nesciunt ad, quos delectus ipsam dolores labore doloribus, doloremque soluta laudantium aliquam magni ut veritatis ipsa.</p>
+        
+            <div class="row pt-5 d-flex flex-row desc">
+                <div class="col-4 d-flex justify-content-center align-items-center imageConteneur">
+                    <img src="images/poulesLibre.gif" alt="bio" style="height: 285px;" class="img-responsive imageRonde">
+                </div>
+                <div class="col-6 d-flex justify-content-center align-items-center p-5">
+                    <div class="card mb-3 textAnim">
+                        <div class="card-header">Boutique</div>
+                        <div class="card-body">
+                            <h5 class="card-title">Information sur la boutique</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quis eius cupiditate quisquam impedit consequatur nihil velit eum illum soluta, corrupti facilis numquam nisi deleniti rem dolorum saepe neque non quas quos! Aut incidunt nesciunt ad, quos delectus ipsam dolores labore doloribus, doloremque soluta laudantium aliquam magni ut veritatis ipsa.</p>
+                        </div>
                     </div>
                 </div>
+                <div class="col-2"></div>
             </div>
-        </div>
-
-        <div class="container pt-5">
-        <div class="row pt-5 d-flex flex-row wrapper">
-            <div class="col-6 d-flex justify-content-center align-items-center">
-                <img src="images/Agriculture_biologique500x500.jpeg" alt="" style="height: 300px;" class="img-responsive imageRonde bg-info">
-            </div>
-            <div class="col-6 d-flex justify-content-center align-items-center p-5">
-                <div class="card text-bg-info mb-3" >
-                    <div class="card-header">Boutique</div>
-                    <div class="card-body">
-                      <h5 class="card-title">Information sur la boutique</h5>
-                      <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quis eius cupiditate quisquam impedit consequatur nihil velit eum illum soluta, corrupti facilis numquam nisi deleniti rem dolorum saepe neque non quas quos! Aut incidunt nesciunt ad, quos delectus ipsam dolores labore doloribus, doloremque soluta laudantium aliquam magni ut veritatis ipsa.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+            
+            
+        
     </div>
 </section>
 
 <div class="container-fluid foodwrap"></div>
 
 
-<?php 
-    include_once "vues/inc/footer.inc.php";
-    include_once "vues/inc/piedPage.inc.php";
+<?php
+include_once "vues/inc/footer.inc.php";
+include_once "vues/inc/piedPage.inc.php";
 ?>
