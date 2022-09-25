37d614a78da73d06a25a42be8778607f17bd1ea3
Martin Forget <mforget2011@hotmail.com>
2022-09-25 02:37:27 -0400

    admin juste pas responsive...

--- fonctionnement.php
+++ fonctionnement.php
@@ -48,8 +48,8 @@ require "vues/inc/navbar.inc.php";
                 <div class="card mb-3 textAnim">
                     <div class="card-header">Employés et bénévoles</div>
                     <div class="card-body">
-                        <h5 class="card-title">Information sur la boutique</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quis eius cupiditate quisquam impedit consequatur nihil velit eum illum soluta, corrupti facilis numquam nisi deleniti rem dolorum saepe neque non quas quos! Aut incidunt nesciunt ad, quos delectus ipsam dolores labore doloribus, doloremque soluta laudantium aliquam magni ut veritatis ipsa.</p>
+                        <h5 class="card-title">Au service des gens et de la planète</h5>
+                        <p>Depuis le tout début, lors de notre ouverture en 2008, bon nombre d'individus se joignent à l'équipe avec, comme mission, l'intention de faire la différence. D'une part, mettre l'épaule à la roue du développement durable et d'autre part, rallier les gens à cette cause qui requiert un mouvement de masse pour se réaliser. </p>
                     </div>
                 </div>
             </div>
